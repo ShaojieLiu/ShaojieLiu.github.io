5515f410f7f394b0dd8b113a5819dfe1aa50b0a9
ShaojieLiu <439655668@qq.com>
2016-12-22 21:19:41 +0800

    https

--- tasklist/task8audioLrc/js/songlist.js
+++ tasklist/task8audioLrc/js/songlist.js
@@ -14,12 +14,12 @@ var getlist = function() {
   [
     {singer: "周杰伦",
       name: '简单爱',
-    	src: 'http://yinyueshiting.baidu.com/data2/music/41526971/107364441482058861128.mp3?xcode=f5d02321a9bb979a8c3c42cb27c397d9',
+    	src: 'https://yinyueshiting.baidu.com/data2/music/41526971/107364441482058861128.mp3?xcode=f5d02321a9bb979a8c3c42cb27c397d9',
     	lrcLink: 'https://musicdata.baidu.com/data2/lrc/13822026/13822026.lrc?_=1482151911775',
       img: '',},
     {singer: "周杰伦",
       name: '半岛铁盒',
-    	src: 'http://yinyueshiting.baidu.com/data2/music/241199568/813234237600128.mp3?xcode=340b1b7cb66ad31737ff48ca31f559c2',
+    	src: 'https://yinyueshiting.baidu.com/data2/music/241199568/813234237600128.mp3?xcode=340b1b7cb66ad31737ff48ca31f559c2',
     	lrcLink: 'https://musicdata.baidu.com/data2/lrc/13854358/13854358.lrc?_=1482151995961',
       img: '',},
   ]
